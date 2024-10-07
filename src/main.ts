97d421250cdb6933e0f75edf9f8f278ff9ffd2fc
Derek Simpson <articunolies22@gmail.com>
2024-10-07 15:03:03 -0700

    Step 1 Complete

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,6 @@ const app: HTMLDivElement = document.querySelector("#app")!;
 const gameName = "My fantastical game";
 document.title = gameName;
 
-
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
