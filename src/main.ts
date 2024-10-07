320986e2b09d22b6ab921011c7ac2df402280b9e
Derek Simpson <articunolies22@gmail.com>
2024-10-07 15:02:39 -0700

    Step 1 Complete

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,12 @@ const app: HTMLDivElement = document.querySelector("#app")!;
 const gameName = "My fantastical game";
 document.title = gameName;
 
+
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+// Add a button
+const button = document.createElement("button");
+button.innerHTML = "🦀";
+app.append(button);
